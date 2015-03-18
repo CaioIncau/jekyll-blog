3144af377dff6ef644ff236aee1a0a3b46798023
Caio Incau <caioincau@gmail.com>
2015-03-17 23:14:42 -0300

    almost there

--- js/toggle.js
+++ js/toggle.js
@@ -1,16 +1,26 @@
 $(function () {
     
+//    $(".visible-bar").click(function () {
+//        var el = $('.main-menu');
+//        el.addClass('main-menu-show');
+//        $(this).addClass('hidden-bar');
+//        $(this).removeClass('visible-bar');
+//    });
     $(".visible-bar").click(function () {
-        var el = $('.main-menu');
-        el.addClass('main-menu-show');
-        $(this).addClass('hidden-bar');
-        $(this).removeClass('visible-bar');
+    var el = $('.main-menu');
+    if (el.hasClass("main-menu-hide"))
+    {
+        el.addClass("main-menu-show").removeClass("main-menu-hide");
+    }
+    else{
+        el.addClass("main-menu-hide").removeClass("main-menu-show");
+    }
     });
-    
-    $(".hidden-bar").click(function () {
-        var el = $('.main-menu');
-        el.removeClass('main-menu-show');
-        $(this).addClass('visible-bar');
-        $(this).removeClass('hidden-bar');
-    });
+
+//    $(".hidden-bar").click(function () {
+//        var el = $('.main-menu');
+//        el.removeClass('main-menu-show');
+//        $(this).addClass('visible-bar');
+//        $(this).removeClass('hidden-bar');
+//    });
 });
