38c528e30d5366c9a9691783afe18725643598de
Caio Incau <caioincau@gmail.com>
2015-03-19 08:22:47 -0300

    responsive fixes

--- js/toggle.js
+++ js/toggle.js
@@ -8,12 +8,21 @@ $(function () {
 //    });
     $(".visible-bar").click(function () {
     var el = $('.main-menu');
+       $(this).removeClass('fa-bars');
     if (el.hasClass("main-menu-hide"))
     {
+
+        $(this).addClass('fa-arrow-left');
+        $(this).removeClass('fa-bars');
+        
         el.css("left","-100%");
         el.addClass("main-menu-show").removeClass("main-menu-hide");
     }
     else{
+
+         $(this).removeClass('fa-arrow-left');
+        $(this).addClass('fa-bars');
+
         el.css("left","0%");
         el.addClass("main-menu-hide").removeClass("main-menu-show");
     }
