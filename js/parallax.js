2eb542e4bff4b0b8012a75ffea03c8ef342da35f
Caio Incau <caioincau@gmail.com>
2015-03-23 20:43:56 -0300

    Extraindo js

--- js/parallax.js
+++ js/parallax.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
+    
+    function scrollTo($div){
+     var yPos = -($(window).scrollTop() / $div.data('speed')); 
+            var bgpos = '50% '+ yPos + 'px'; 
+           $div.css('background-position', bgpos );
+    };
  
    $('section.bgParallax').each(function(){
     	var $obj = $(this);
        	$(window).scroll(function() {
-            var yPos = -($(window).scrollTop() / $obj.data('speed')); 
-            var bgpos = '50% '+ yPos + 'px'; 
-           $obj.css('background-position', bgpos );
+            scrollTo($obj);
         }); 
  
    });	
